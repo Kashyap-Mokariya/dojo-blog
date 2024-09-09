b49e7162b352019e99aca489aa47dd7e35e23c10
Kashyap Mokariya <mokariyakashyap@gmail.com>
2024-09-09 22:29:42 +0530

    updated

--- src/Home.js
+++ src/Home.js
@@ -70,12 +70,20 @@
 // export default Home;
 
 
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
+    const { data, isLoading, error } = useFetch('http://localhost:8000/blogs')
 
-    
+    return (
+        <div className="home">
+            {error && <div>{error}</div>}
+            {isLoading && <div>Loading...</div>}
+            {data && <BlogList blogs={data} />}
+        </div>
+    );
 }
 
 export default Home;
